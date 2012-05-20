2e9eccb159578043e5903d3533c2bb7c4f19f1b9
Joe Baker <jbaker@alum.wpi.edu>
2012-05-20 02:17:43 -0400

    Category request methods.
    Added most of the error codes we will likely need.
    
    Signed-off-by: Joe Baker <jbaker@alum.wpi.edu>

--- octopart.py
+++ octopart.py
@@ -1,9 +1,12 @@
 import urllib2
 import json
-import types
+from types import *
 
 class OctopartException(Exception):
-	errors = {0: 'Required argument missing from method call.'}
+	errors = {0: 'Required argument missing from method call.', \
+			  1: 'Passed an invalid argument for this method.', \
+			  2: 'Malformed argument.', \
+			  3: 'An argument was passed more than once.'}
 	def __init__(self, source, args, error_number):
 		self.source = source
 		self.arguments = args
@@ -14,7 +17,8 @@ class OctopartException(Exception):
 		return repr(str)
 
 class Octopart:
-	''' A simple client frontend to tho Octopart public REST API. '''
+	''' A simple client frontend to tho Octopart public REST API. 
+	For detailed API documentation, refer to http://octopart.com/api/documentation'''
 	api_url = 'http://octopart.com/api/v2/'
 	
 	def __init__(self, api_key=None):
@@ -42,4 +46,78 @@ class Octopart:
 		response = urllib2.urlopen(req_url).read() 
 		json_obj = json.loads(response)
 		return json_obj
+	
+	# TODO: JSON list arguments will be passed as a python list, need to convert to string?
+	#if type(arg val) in arg_types[arg]
+	def categories_get(self, args):
+		''' Fetch a category object by its id. '''
+		method = 'categories/get'
+		required_args = frozenset('id',)
+		arg_types = {'id': StringType}
+		valid_args = frozenset(arg_types.keys())
+		args_set = set(args.keys())
+		
+		if required_args.issubset(args_set) is False:
+			raise OctopartException(self.categories_get.__name__, args, 0)
+		if args_set.issuperset(valid_args):
+			raise OctopartException(self.categories_get.__name__, args, 1)
+		for key in args_set:
+			if arg_types[key] is StringType:
+				if isinstance(args[key], basestring) is False:
+					raise OctopartException(self.categories_get.__name__, args, 2)
+			else:
+				if type(args[key]) is not arg_types[key]:
+					raise OctopartException(self.categories_get.__name__, args, 2)
+		if len(args_set) != len(args.keys()):
+			raise OctopartException(self.categories_get.__name__, args, 3)
+		
+		return self.__get(method, args)
+	
+	def categories_get_multi(self, args):
+		''' Fetch multiple category objects by their ids. '''
+		method = 'categories/get_multi'
+		required_args = frozenset('ids',)
+		arg_types = {'ids': ListType}
+		valid_args = frozenset(arg_types.keys())
+		args_set = set(args.keys())
+		
+		if required_args.issubset(args_set) is False:
+			raise OctopartException(self.categories_get_multi.__name__, args, 0)
+		if args_set.issuperset(valid_args):
+			raise OctopartException(self.categories_get_multi.__name__, args, 1)
+		for key in args_set:
+			if arg_types[key] is StringType:
+				if isinstance(args[key], basestring) is False:
+					raise OctopartException(self.categories_get_multi.__name__, args, 2)
+			else:
+				if type(args[key]) is not arg_types[key]:
+					raise OctopartException(self.categories_get_multi.__name__, args, 2)
+		if len(args_set) != len(args.keys()):
+			raise OctopartException(self.categories_get_multi.__name__, args, 3)
+		
+		return self.__get(method, args)
+	
+	def categories_search(self, args):
+		''' Execute search over all category objects. '''
+		method = 'categories/search'
+		required_args = frozenset()
+		arg_types = {'q': StringType, 'start' : IntType, 'limit' : IntType, 'ancestor_id' : IntType}
+		valid_args = frozenset(arg_types.keys())
+		args_set = set(args.keys())
+		
+		if required_args.issubset(args_set) is False:
+			raise OctopartException(self.categories_search.__name__, args, 0)
+		if args_set.issuperset(valid_args):
+			raise OctopartException(self.categories_search.__name__, args, 1)
+		for key in args_set:
+			if arg_types[key] is StringType:
+				if isinstance(args[key], basestring) is False:
+					raise OctopartException(self.categories_search.__name__, args, 2)
+			else:
+				if type(args[key]) is not arg_types[key]:
+					raise OctopartException(self.categories_search.__name__, args, 2)
+		if len(args_set) != len(args.keys()):
+			raise OctopartException(self.categories_search.__name__, args, 3)
+		
+		return self.__get(method, args)
 
